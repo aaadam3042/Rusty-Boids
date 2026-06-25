988813d518aac4a2d27d3370924002efc43712ac
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2026-06-25 15:59:11 +1000

    Added helper function to generate a random index

--- crates/boids-core/src/math.rs
+++ crates/boids-core/src/math.rs
@@ -1,6 +1,10 @@
 use core::ops::{Add, Sub, Mul, Div};
 use rand_core::Rng;
 
+pub fn random_index<R: Rng>(rng: &mut R, length: u32) -> u32 {
+    (rng.next_u32()) % length
+}
+
 pub fn random_range_f32<R: Rng>(rng: &mut R, min:f32, max: f32) -> f32 {
     min + (rng.next_u32() as f32 / core::u32::MAX as f32) * (max - min)
 }
