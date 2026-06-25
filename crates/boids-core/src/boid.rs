9f49d196d713e62f0fab02343f039927ec99c4d0
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2026-06-25 12:51:38 +1000

    Refactored random_in from Boid to World

--- crates/boids-core/src/boid.rs
+++ crates/boids-core/src/boid.rs
@@ -8,16 +8,7 @@ pub struct Boid {
 }
 
 impl Boid {
-    fn new(position: Vec2, velocity: Vec2) -> Self {
+    pub fn new(position: Vec2, velocity: Vec2) -> Self {
         Boid {position: position, velocity: velocity}
-    }
-
-    pub fn random_in<R: Rng>(rng: &mut R, area_size: Vec2) -> Self {
-        let x = math::random_range_f32(rng, 0.0, area_size.x);
-        let y = math::random_range_f32(rng, 0.0, area_size.y);
-        Self::new(
-            Vec2::new(x, y),
-            Vec2::ZERO
-        )
     }
 }
--- crates/boids-core/src/world.rs
+++ crates/boids-core/src/world.rs
@@ -1,6 +1,6 @@
 use rand_core::Rng;
 
-use crate::boid::{self, Boid};
+use crate::boid::Boid;
 use crate::math::{self, Vec2};
 use crate::params::{SimulationParams, WorldParams};
 
@@ -12,32 +12,42 @@ pub struct World {
 
 impl World {
     fn new<R: Rng>(rng:&mut R, sim_params: SimulationParams, world_params: WorldParams) -> Self {
-        let boids = boid::init_boids(rng, world_params.area_size, world_params.boid_count);
+        let mut world = Self {boids: Vec::new(), sim_params: sim_params, world_params: world_params};
+        world.init_boids(rng);
 
-        Self {boids: boids, sim_params: sim_params, world_params: world_params}
+        world
     }
-
-    fn init_boids<R: Rng>(rng: &mut R, area_size: Vec2, boid_count: usize) -> Vec<Boid> {
-    // Generate a given amount of boids in random positions
-    let mut boids = Vec::<Boid>::new();
-
-    let angle = math::random_range_f32(rng, 0.0, core::f32::consts::TAU);
-    let speed = math::random_range_f32(rng, min, max); // How to load max speed in
-
-    let start_velocity = Vec2::new(angle.cos(), angle.sin()) * speed;
-    // We dont actually want to use sin and cos if we are dealing with embedded. there may be
-    // implementations on esp32 hal but maybe we should try with a small direction lookup instead
-    // As for our purposes we dont really need the full set of random directions anyway
-    // Eventually the boids diverge and converge
-
-    for _ in 0..boid_count {
-        boids.push(Boid::random_in(rng, area_size)); 
-    }
-    boids
-}
-
+    
     fn new_default_params<R: Rng>(rng:&mut R) -> Self {
         Self::new(rng, SimulationParams::default(), WorldParams::default())
+    }
+
+    fn init_boids<R: Rng>(&mut self, rng: &mut R) {
+        // Generate a given amount of boids in random positions
+        self.boids.clear();
+
+        for _ in 0..self.world_params.boid_count {
+            let boid = self.random_boid(rng);
+            self.boids.push(boid); 
+        }
+    }
+
+    fn random_boid<R: Rng>(&mut self, rng: &mut R) -> Boid {
+        let angle = math::random_range_f32(rng, 0.0, core::f32::consts::TAU);
+        let min_speed = self.sim_params.max_speed * 0.4;
+        let speed = math::random_range_f32(rng, min_speed, self.sim_params.max_speed); // How to load max speed in
+
+        let start_velocity = Vec2::new(angle.cos(), angle.sin()) * speed;
+        // We dont actually want to use sin and cos if we are dealing with embedded. there may be
+        // implementations on esp32 hal but maybe we should try with a small direction lookup instead
+        // As for our purposes we dont really need the full set of random directions anyway
+        // Eventually the boids diverge and converge
+
+        let area_size = self.world_params.area_size;
+        let x = math::random_range_f32(rng, 0.0, area_size.x);
+        let y = math::random_range_f32(rng, 0.0, area_size.y);
+
+        Boid::new(Vec2::new(x, y), start_velocity)
     }
 
     fn tick(&mut self, dt: i32) {
