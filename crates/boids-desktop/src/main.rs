b8a88e925e2405e89097b30904fe1358282fab63
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2026-07-29 16:56:48 +1000

    Implemented fixed frame logic

--- crates/boids-desktop/src/main.rs
+++ crates/boids-desktop/src/main.rs
@@ -1,36 +1,54 @@
 use eframe::egui;
+use std::time::Instant;
 
 const FIXED_DT: f32 = 1.0/120.0;
+const MAX_FRAME_TIME: f32 = 0.1;
 
 fn main() -> eframe::Result {
     eframe::run_native(
         "Boids Desktop App",
         eframe::NativeOptions::default(),
         Box::new(
-            |cc| Ok(Box::new(BoidsDesktopApp::new(cc)))
+            |_cc| Ok(Box::new(BoidsDesktopApp::new()))
         ),
     )
 }
 
 struct BoidsDesktopApp {
     world: boids_core::World,
-    previous_time: std::time::Instant,
+    previous_time: Instant,
     accumulator: f32,
 }
 
 impl BoidsDesktopApp {
-    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+    fn new() -> Self {
         let mut rng = rand::rng();
 
         Self {
             world: boids_core::World::new_default_params(&mut rng),
+            previous_time: Instant::now(),
+            accumulator: 0.0,
         }
     }
 }
 
 impl eframe::App for BoidsDesktopApp {
-    fn logic(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        
+    fn logic(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let now = Instant::now();
+
+        let elapsed = now.duration_since(self.previous_time).as_secs_f32();
+
+        self.previous_time = now;
+
+        let elapsed = elapsed.min(MAX_FRAME_TIME);
+        self.accumulator += elapsed;
+
+        while self.accumulator >= FIXED_DT {
+            self.world.tick(FIXED_DT);
+            self.accumulator -= FIXED_DT;
+        }
+
+        ctx.request_repaint();
     }
 
     fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
