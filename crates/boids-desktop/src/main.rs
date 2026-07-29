9e5889a8630fe1fa00cffa273050e1823a713ae1
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2026-07-29 16:23:54 +1000

    Added rng

--- crates/boids-desktop/src/main.rs
+++ crates/boids-desktop/src/main.rs
@@ -1,31 +1,39 @@
 use eframe::egui;
 
+const FIXED_DT: f32 = 1.0/120.0;
 
-fn main() {
+fn main() -> eframe::Result {
     eframe::run_native(
         "Boids Desktop App",
         eframe::NativeOptions::default(),
         Box::new(
             |cc| Ok(Box::new(BoidsDesktopApp::new(cc)))
         ),
-    );
+    )
 }
 
 struct BoidsDesktopApp {
-    // world: boids_core::World,
+    world: boids_core::World,
+    previous_time: std::time::Instant,
+    accumulator: f32,
 }
 
 impl BoidsDesktopApp {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
-    //     Self {
-    //         world: boids_core::World::new_default_params(),
-    //     }
-        Self {}
+        let mut rng = rand::rng();
+
+        Self {
+            world: boids_core::World::new_default_params(&mut rng),
+        }
     }
 }
 
 impl eframe::App for BoidsDesktopApp {
-    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame) {
+    fn logic(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        
+    }
+
+    fn ui(&mut self, ui: &mut egui::Ui, _frame: &mut eframe::Frame) {
         ui.label("Boids Desktop App");
         // Here you can add more UI elements to control the simulation
     }
